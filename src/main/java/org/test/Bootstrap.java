ef45cb40ee87652c45110ff4993c7978da9f3294
Martin Vysny <martin@vysny.me>
2018-09-06 10:04:57 +0300

    Added javadoc

--- src/main/java/org/test/Bootstrap.java
+++ src/main/java/org/test/Bootstrap.java
@@ -1,6 +1,5 @@
 package org.test;
 
-import com.github.vok.framework.VaadinOnKotlin;
 import com.github.vokorm.DBKt;
 import com.github.vokorm.VokOrm;
 
@@ -15,6 +14,7 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 /**
+ * Bootstraps the VokOrm so that the SQLDataProvider has access to the database.
  * @author mavi
  */
 @WebListener
