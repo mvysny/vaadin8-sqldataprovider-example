86ee6b43a4747737eb8d83e5abc182e6c663baad
Martin Vysny <martin@vysny.me>
2018-11-09 07:51:57 +0200

    v bump

--- src/test/java/org/test/MyUITest.java
+++ src/test/java/org/test/MyUITest.java
@@ -1,16 +1,12 @@
 package org.test;
 
-import com.github.karibu.testing.MockVaadin;
+import com.github.mvysny.kaributesting.v8.MockVaadin;
 import com.vaadin.ui.Grid;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static com.github.karibu.testing.GridKt.*;
-import static com.github.karibu.testing.LocatorJ.*;
+import static com.github.mvysny.kaributesting.v8.GridKt.*;
+import static com.github.mvysny.kaributesting.v8.LocatorJ.*;
 
 /**
  * Look Ma, no Spring/JavaEE/Servlet container necessary! That's what you get when you keep things simple.
